61db3790d80028a4b739e179399e0b38b8fa4941
duytan0502 <tann14195@gmail.com>
2026-05-28 14:16:49 +0700

    28/5

--- Module/header.php
+++ Module/header.php
@@ -205,7 +205,7 @@ $prefix = strpos($scriptPath, '/Pages/') !== false ? '..' : '.';
             </li>
             <li class="menu_items" style="font-size: 18px">
                 <a href="../Pages/KhuyenMai.php"><strong>Khuyến mãi</strong></a>
-            </li>>
+            </li>
             <li class="menu_items" style="font-size: 18px">
                 <a href="../Pages/trangLienHe.php"><strong>Liên hệ</strong></a>
             </li>
